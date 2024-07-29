3156bb47cd8dbb106a9498ec359f3533051fa171
pasko.paskov <pasko@stepsoft.bg>
2024-07-29 15:43:15 +0300

    Remove var_dumps

--- tests/testFilesAPI.php
+++ tests/testFilesAPI.php
@@ -7,8 +7,8 @@ test('Test FilesAPI', function () {
     
     //Replace Token with your own
     $client = new SOfficeRequest(
-        'http://192.168.0.106:8081/', 
-        //'https://soffice8api.stepsoft.bg/', 
+        //'http://192.168.0.106:8081/', 
+        'https://soffice8api.stepsoft.bg/', 
         'v1', 
         'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJwYXNrb0BzdGVwc29mdC5iZyIsInN1YiI6IkFQSSIsImRhdGFiYXNlIjoiU09mZmljZThfU19EZXZlbG9wZXJOZXcyMDI0MDcxNiIsImV4cCI6MTcyMTk5Nzk3OS4wOTEyMzR9.SvxH7AjzCUUb5B8iQcIc1zLNQV8ZEElUv-YbqSbgD-g');
     
@@ -19,7 +19,7 @@ test('Test FilesAPI', function () {
         'nomenclature',
     );
 
-    var_dump($response);
+    //var_dump($response);
 
     // Ensure that the response is an object
     expect($response)->toBeArray();
--- tests/testNmclAPI.php
+++ tests/testNmclAPI.php
@@ -7,17 +7,17 @@ test('Test NmclAPI', function () {
     
     //Replace Token with your own
     $client = new SOfficeRequest(
-        //'http://192.168.0.106:8081/', 
-        'https://soffice8api.stepsoft.bg/', 
+        'http://192.168.0.106:8081/', 
+        //'https://soffice8api.stepsoft.bg/', 
         'v1', 
-        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJwYXNrb0BzdGVwc29mdC5iZyIsInN1YiI6IkFQSSIsImRhdGFiYXNlIjoiU09mZmljZThfU19EZXZlbG9wZXJOZXciLCJleHAiOjE3MjE5ODM1MjcuOTg1NDYxfQ.pCkWJrvpSKY0_SfHduA_Op5Rcpwg9lWtuIKm4i4EjI8');
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJwYXNrb0BzdGVwc29mdC5iZyIsInN1YiI6IkFQSSIsImRhdGFiYXNlIjoiU09mZmljZThfU19EZXZlbG9wZXJOZXciLCJleHAiOjE3MjIwMTUxMTYuMDg3Mzk5fQ.lU_QKpQHKQqLiGUUiqFgUOvb_flUg--4vUeXFpX754c');
     
     $testNmclAPIRequest = new NmclAPI($client);
 
     $response = $testNmclAPIRequest->getNmclRange(
         17,
-        30346,
-        30355,
+        5667,
+        5767,
         //27,
         //10,
         //100,
