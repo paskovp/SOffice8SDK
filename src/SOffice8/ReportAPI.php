777bad211ab43b6e939deb435d6c4c4f46558a69
pasko.paskov <pasko@stepsoft.bg>
2025-10-28 14:26:06 +0200

    Changes for reports result filtering

--- src/SOffice8/ReportAPI.php
+++ src/SOffice8/ReportAPI.php
@@ -20,23 +20,24 @@ class ReportAPI extends BaseAPI {
         parent::__construct($client);
     }
 
-    public function getReportResultAPI($id_report, $id_report_setting, $requestedrows, $startpage, $rows, $page, $config = true) {
+    public function getReportResultAPI($id_report, $id_report_setting, $requestedrows, $startpage, $rows, $page, $config = true, $filters = null, $sortbycolumn = null, $sortdirection = null) {
+        
+        if ($config) {
+            $response1 = $this->client->sendRequest('reports/config/'.$id_report, 'GET');
+        }
 
         $data = [
             'id_report' => $id_report, //id of the report to get the records from
             'id_report_setting' => $id_report_setting, //id of the report to get the records from
-            'requestedrows' => $requestedrows, //number of rows requested
-            'startpage' => $startpage, //start page
-            'rows' => $rows, //number of rows
-            'page' => $page //page number
+            'requestedrows' => $requestedrows, //number of rows requested, if null, all rows will be returned
+            'startpage' => $startpage, //start page, if null, all pages will be returned
+            'rows' => $rows, //number of rows, if null, all rows will be returned
+            'config' => $config, //config parameter (true/false) - if true, it will return the configuration of the report
+            'filters' => $filters, //filters array like ['filter1' => 'value1', 'filter2' => 'value2'], if null, no filters will be applied
+            'sortbycolumn' => $sortbycolumn, //sort by column like 'id', if null, no sorting will be applied
+            'sortdirection' => $sortdirection //sort direction, asc by default, can be 'desc', if null, no sorting will be applied
         ];
     
-        if ($config) {
-            $response1 = $this->client->sendRequest('reports/config/'.$id_report, 'GET');
-            // var_dump($response1);
-            // Return settings loaded from the report configuration
-            // return $this->validate($response1);
-        }
 
         $response2 = $this->client->sendRequest('reports/reportAPI', 'POST', $data);
 
